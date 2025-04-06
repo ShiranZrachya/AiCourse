c3e15ccd6b0a0ab30fe57a6a662efe0039cb9099
ShiranZrachya <45419258+ShiranZrachya@users.noreply.github.com>
2025-04-06 14:40:39 +0700

    first intro commit

---
+++ intro/app.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import OpenAi from "openai";
+
+const openai = new OpenAi();
+
+const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    messages: [
+      {
+        role: "user",
+        content: "Hey, my name is Ariel, greet me.",
+      }
+    ]
+  });
+  
+  console.log(response.choices);
+
